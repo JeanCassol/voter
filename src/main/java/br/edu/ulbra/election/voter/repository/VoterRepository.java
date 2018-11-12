b02935fbf78616f61fb5a3f5aa6009b73b55f43d
Jean Michello Cassol Castro <jean_cassol@yahoo.com.br>
2018-11-12 17:58:03 -0200

    v.0.2.0

--- src/main/java/br/edu/ulbra/election/voter/repository/VoterRepository.java
+++ src/main/java/br/edu/ulbra/election/voter/repository/VoterRepository.java
@@ -1,7 +1,11 @@
 package br.edu.ulbra.election.voter.repository;
 
-import br.edu.ulbra.election.voter.model.Voter;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
+import br.edu.ulbra.election.voter.model.Voter;
+
 public interface VoterRepository extends CrudRepository<Voter, Long> {
+	List<Voter> findByEmail(String email);
 }
